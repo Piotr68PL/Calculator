2d5f0947c4298601e58ec76c83f369b72c9260b7
Piotr68PL <rogg.piotr@gmail.com>
2018-07-19 18:56:35 +0200

    Updated Code

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kalkulator
 {
@@ -11,7 +7,7 @@ namespace Kalkulator
         static void Main(string[] args)
         {
             #region Welcome_Screen
-            Console.WriteLine("Welcome to my first calculator. You must type 'none' of the end. For first enter number of numbers, that you want to calculate");
+            Console.WriteLine("Welcome to my first calculator. For first enter number of numbers, that you want to calculate");
             string num = Console.ReadLine();
             int numofoper = Int32.Parse(num);
             #endregion
@@ -22,34 +18,37 @@ namespace Kalkulator
                 Console.WriteLine("Enter number");
                 string num2 = Console.ReadLine();
                 int numint2 = Int32.Parse(num2);
-                Console.WriteLine("Choose operation (+, -, *, / or none):");
-                string operation = Console.ReadLine();
-                switch (operation)
+                if (numofoper != i+1)
                 {
-                    case "+":
-                        calc = numint2 + calc;
-                        break;
-                    case "-":
-                        calc = numint2 - calc;
-                        break;
-                    case "*":
-                        calc = numint2 * calc;
-                        break;
-                    case "/":
-                        calc = numint2 / calc;
-                        break;
-                    case "none":
-                        break;
-                    default:
-                        Console.WriteLine("ERROR");
-                        break;
+                    Console.WriteLine("Choose operation (+, -, *, /):");
+                    string operation = Console.ReadLine();
+                    switch (operation)
+                    {
+                        case "+":
+                            calc = numint2 + calc;
+                            break;
+                        case "-":
+                            calc = numint2 - calc;
+                            break;
+                        case "*":
+                            calc = numint2 * calc;
+                            break;
+                        case "/":
+                            calc = numint2 / calc;
+                            break;
+                        default:
+                            Console.WriteLine("ERROR");
+                            break;
+                    }
                 }
             }
+            #endregion
+            #region Result
             Console.WriteLine("Your result is: " + calc);
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
             #endregion
-            
+
 
         }
     }
